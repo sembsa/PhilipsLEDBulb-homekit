bb0c00079c76a4772c22e7a0c50e752f9266113a
Sebastian Trześniewski <sembsa@me.com>
2017-08-24 23:44:09 +0200

    init
    First load to github.com

---
+++ philips.py
@@ -0,0 +1,76 @@
+import mirobo
+import paho.mqtt.client as mqtt
+
+print("Skrypt sterowania Philips Xiaomi LED Bulb")
+
+# Inicjacja zarowki z biblioteki mirobo
+led = mirobo.Ceil()
+led.off()
+
+# Set MQTT Topic
+setOnMQTT = "/home/setOn"
+statusOnMQTT = "/home/statusOn"
+setBrightnessMQTT = "/home/setB"
+statusBrightness = "/home/statusB"
+setColorTemperatureMQTT = "/home/setColor"
+statusColorTemperature = "/home/statusColorTemperature"
+
+# Metody Paho-MQTT
+
+# Gdy polaczony
+def on_connect(client, userdata, flasgs, rc):
+    print("Connect with result code: " + str(rc))
+
+    client.subscribe(setOnMQTT)
+    client.subscribe(setBrightnessMQTT)
+    client.subscribe(setColorTemperatureMQTT)
+
+
+# Gdy odbiera wiadomosc
+def on_message(client, userdata, message):
+    if message.topic == setOnMQTT:
+        on = bool(int(message.payload))
+        if on == True:
+            setPowerOn(True)
+        else:
+            setPowerOn(False)
+    if message.topic == setBrightnessMQTT:
+        setBrightness(int(message.payload))
+    if message.topic == setColorTemperatureMQTT:
+        print(int(message.payload))
+        setColorTemperature(int(message.payload))
+
+# -------------------------
+# Metody sterowania zarowka
+def setPowerOn(on):
+    if on == True:
+        print("<INFO> Philips Xiaomi LED Bulb ON: True")
+        led.on()
+        client.publish(statusOnMQTT, 1)
+    else:
+        print("<INFO> Philips Xiaomi LED Bulb ON: False")
+        led.off()
+        client.publish(statusOnMQTT, 0)
+
+def setBrightness(value):
+    print("<INFO> Philips Xiaomi LED Bulb Brightness: " + str(value))
+    led.set_bright(value)
+    client.publish(statusBrightness, value)
+
+def setColorTemperature(value):
+    print("<INFO> Philips Xiaomi LED CCT: " + str(value))
+    led.set_cct(calculateColorTemperature(value, 140, 500, 100, 1))
+    client.publish(statusColorTemperature, value)
+
+# Metoda obliczania map
+def calculateColorTemperature(x, in_min, in_max, out_min, out_max):
+    result = (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+    return result
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("homekit.local", 1883, 60)
+
+client.loop_forever()
